0755e7946c5a678171ce9efcd7cf76b97b427998
Spikehero <44679068+Spikehero@users.noreply.github.com>
2018-11-01 21:52:28 +0300

    Add project

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 124);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Площадь основания свалки, м2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Высота свалки, м";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Форма свалки";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(20, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 65);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Удалённость от мест жизнедеятельности человека, водоёмов и ООПТ, м";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(20, 278);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 50);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Время существования свалки, лет";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(20, 338);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 55);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Вид поверхности, на которой расположена свалка";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "конус",
+            "пирамида",
+            "параллелепипед"});
+            this.comboBox1.Location = new System.Drawing.Point(110, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "...";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "пищевые отходы",
+            "бумага, металл",
+            "текстиль, шерсть",
+            "дерево, кожаные изделия",
+            "кирпич/камень"});
+            this.comboBox2.Location = new System.Drawing.Point(116, 176);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(137, 227);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(137, 290);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox3.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.ItemHeight = 13;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Металлические крыши с уклоном",
+            "Металлические крыши плоские",
+            "Дороги бетонные, асфальт",
+            "Поверхность жидких отходов в картах",
+            "Дороги грунтовые",
+            "Грунтовые поверхности (спланированные)",
+            "Щебёночные покрытия",
+            "Откосы насыпей, отвалов, дамб",
+            "Неспланированные неуплотнённые отвалы без растительного покрова",
+            "Насыпные поверхности, покрытые травянистой и редкой кустарниковой растительностью" +
+                "",
+            "Акватории отстойников со слабозагрязнённой поверхностью"});
+            this.comboBox3.Location = new System.Drawing.Point(126, 352);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(174, 21);
+            this.comboBox3.TabIndex = 14;
+            this.comboBox3.Text = "...";
+            this.comboBox3.MouseHover += new System.EventHandler(this.comboBox3_MouseHover);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(23, 416);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(108, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Объём фильтрата =";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(137, 416);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "0";
+            this.label10.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(327, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 46);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Далее";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(26, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 54);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Среднее количество осадков за год, мм";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Состав отходов";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(116, 133);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            650,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            440,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 22;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            440,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+
+    public partial class Form1 : Form
+    {
+        int h1 = 440,IndexY = 0 ;
+        double S, H, V, K1, AO, BHO, OB, Kov = 0.5;
+        string selection1;
+
+        static double[][] osad = new double[][] {           //   Массив с коэффицентами для осадков
+            new double[] { 440, 1.37 },
+            new double[] { 450, 1.38 },
+            new double[] { 460, 1.39 },
+            new double[] { 470, 1.40 },
+            new double[] { 480, 1.40 },
+            new double[] { 490, 1.42 },
+            new double[] { 500, 1.42 },
+            new double[] { 510, 1.43 },
+            new double[] { 520, 1.44 },
+            new double[] { 530, 1.44 },
+            new double[] { 540, 1.45 },
+            new double[] { 550, 1.46 },
+            new double[] { 560, 1.47 },
+            new double[] { 570, 1.47 },
+            new double[] { 580, 1.48 },
+            new double[] { 590, 1.48 },
+            new double[] { 600, 1.49 },
+            new double[] { 610, 1.49 },
+            new double[] { 620, 1.50 },
+            new double[] { 630, 1.51 },
+            new double[] { 640, 1.51 },
+            new double[] { 650, 1.51 },
+        };
+
+        static double[] vid = new double[]          //   массив с коэффицентами для видов поверхностей
+        {0.20, 0.30, 0.40, 0.50, 0.50, 0.56, 0.58, 0.60, 0.85, 0.90, 0.90};
+
+        static double GetV(double S, double H = 0, string selection1 = "")          //   метод для нахождения объёма
+        {
+            switch (selection1)         //   свич, выбирающий форму
+            {
+                case "конус":
+                    return (S * H) / 3;
+                    //break;
+
+                case "пирамида":
+                    return (S * H) / 3;
+                   // break;
+
+                case "параллелепипед":
+                    return (S * H);
+                   // break;
+
+                default:
+                    return 0;
+                  //  break;
+            }
+        }
+
+        private void comboBox3_MouseHover(object sender, EventArgs e)           //   разворачивается список при наведении мыши
+        {
+            var box = sender as ComboBox;
+            box.DroppedDown = true;
+        }
+
+    
+
+        static int GetIndex(int h1)         //   получения индекса для коэффицента осадков
+        {
+            for (int i=0; i<22; i++)
+            {
+                if (osad [i][0] == h1)
+                    {
+                    return i;
+                    }
+               // IndexY = GetIndex(h1);
+            }
+
+            return 0;    
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)           //   получения переменной площади
+        {
+            S = double.Parse(textBox1.Text);
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)          //   получение данных о форме при клавиатурном вводе
+        {
+            selection1 = comboBox1.Text.ToString();
+        }
+            
+        private void textBox2_TextChanged(object sender, EventArgs e)           //   получение переменной высоты
+        {
+            H = double.Parse(textBox2.Text);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)         //  получение данных о форме при выборе из списка
+        {
+            selection1 = comboBox1.SelectedItem.ToString();
+           // MessageBox.Show(selection1);
+        }
+              
+                public Form1()          //  здраствуй, форма
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)          //   кнопка 
+        {
+
+            V = GetV(S, H, selection1);         //   получаем объём свалки
+            BHO = (0.15 * V);
+            
+            
+            h1 = Convert.ToInt32(numericUpDown1.Text);          //   получаем значение из нумерика
+            IndexY = GetIndex(h1);          //   получаем индекс коэффицента осадков
+            K1 = osad[IndexY][1];           //   находим коэффицент
+            // h1 = numericUpDown1.Value;
+            AO = 0.001 * S * h1 * K1;
+           // OB = Kov * (AO - IC);
+            label10.Text = Convert.ToString(BHO);
+            label10.Visible = true;
+            button2.Visible = true;
+           // label9.Text = Convert.ToString(vid [0]);
+            label9.Text = Convert.ToString(comboBox3.Items.IndexOf(comboBox3.Text));
+        }
+    }
+}
